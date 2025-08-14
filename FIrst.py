e70bbb5c6f01401c7e59db75df0032666da8ec3f
suriashu <suriashu>
2025-08-14 11:19:21 +0000

    First Commit

---
+++ FIrst.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+SELECT *
+FROM `system`.`compute`.`clusters`
+WHERE account_id = '101'
+  AND workspace_id = '01';
+
+# COMMAND ----------
+
+i want to read csv first row is a header can you please provide code for that
+
+
+# COMMAND ----------
+
+print('Python Prog')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "run sql"
+
+# COMMAND ----------
+
+
